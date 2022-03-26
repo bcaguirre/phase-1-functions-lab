05f9d5fb327b430e58e9277dabc7ea05a7989259
bernadette <aguirre.bernadette@outlook.com>
2022-03-25 19:26:47 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber){
+   const streetNumber2 = 42
+    return Math.abs(streetNumber-42)
+}
+   
+function distanceFromHqInFeet(streetNumber){
+    const blocks = distanceFromHqInBlocks(streetNumber);
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(streetNumber, streetNumber2){
+    return Math.abs(streetNumber-streetNumber2)* 264
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400){
+        return 0;
+    } else if (feet <= 2000){
+        return 2.56;
+    } else if (feet > 2000 && feet <= 2500){
+        return 25;
+    } else if (feet >= 2500){
+        return 'cannot travel that far';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
